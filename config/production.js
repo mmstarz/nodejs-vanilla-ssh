d2f0a995c25428baa8e09fadcec68dc172b6b5f9
mmstarz <frace@ukr.net>
2019-10-23 10:29:55 +0300

    heroku env credentials updated

--- config/production.js
+++ config/production.js
@@ -1,7 +1,6 @@
 module.exports = {
-  darkskySecretKey: "c21fbde7cfbc625a9ef3c02965711ef5",
+  darkskySecretKey: process.env.DARKSKY_SECRET_KEY,
   units: "si",
   language: "en",
-  mapboxPublicKey:
-    "pk.eyJ1IjoibW1zdGFyIiwiYSI6ImNrMXJqdXR1NDA1MXEzY201Ymx0dTI1a2YifQ.HTKJYU90llResdBKu6PiLg"
+  mapboxPublicKey: process.env.MAPBOX_PUBLIC_KEY    
 };
